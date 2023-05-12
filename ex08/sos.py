3c7e5ed62c7413f0b5751d5b3674dfcde506c5d3
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-12 10:12:44 +0200

    Add program base for ex08

---
+++ ex08/sos.py
@@ -0,0 +1,27 @@
+from sys import argv
+
+if len(argv) == 1:
+    exit(0);
+
+MORSE = {
+    "A": "", "B": "", "C": "", "D": "", "E": "", "F": "",
+    "G": "", "H": "", "I": "", "J": "", "K": "", "L": "",
+    "M": "", "N": "", "O": "", "P": "", "Q": "", "R": "",
+    "S": "", "T": "", "U": "", "V": "", "W": "", "X": "",
+    "Y": "", "Z": "", "0": "", "1": "", "2": "", "3": "",
+    "4": "", "5": "", "6": "", "7": "", "8": "", "9": "",
+}
+
+s = ' '.join(argv[1:])
+codes = []
+
+for c in s:
+    morse = MORSE.get(c.upper(), None)
+
+    if morse is None:
+        print("ERROR")
+        exit(1)
+
+    codes += morse
+
+print(' '.join(code))
