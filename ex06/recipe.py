20fd2a670e574cc8adf89b9ed6dc02d7746953cd
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-12 09:52:26 +0200

    Make recipe.py a command line executable

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -35,7 +35,7 @@ def print_recipe_details():
     prep_time = recipe["prep_time"]
 
     print(f"Recipe for {name}:")
-    print(f"  Ingredients list: {recipe}")
+    print(f"  Ingredients list: {ingredients}")
     print(f"  To be eaten for {meal}.")
     print(f"  Takes {prep_time} minutes of cooking.")
 
@@ -81,7 +81,7 @@ def add_recipe():
         try:
             prep_time = int(ipt)
 
-            if prep_time > 0:
+            if prep_time >= 0:
                 break
         except:
             pass
@@ -91,3 +91,42 @@ def add_recipe():
         "meal": meal,
         "prep_time": prep_time,
     }
+
+def quit_cookbook():
+    print("Cookbook closed. Goodbye!")
+    exit(0)
+
+options = {
+    "1": ("Add a recipe", add_recipe),
+    "2": ("Delete a recipe", delete_recipe),
+    "3": ("Print a recipe", print_recipe_details),
+    "4": ("Print the cookbook", print_recipe_names),
+    "5": ("Quit", quit_cookbook),
+}
+
+def usage():
+    print("List of available option:")
+
+    for k, (desc, _) in options.items():
+        print(f"{k}: {desc}")
+
+print("Welcome to the Python Cookbook !")
+usage()
+
+while True:
+    print()
+    print ("Please select an option:")
+    choice = input(">> ")
+    
+    print()
+
+    opt = options.get(choice, None)
+
+    if opt is None:
+        print("Sorry, this option does not exist.")
+        usage()
+        continue
+
+    _, func = opt
+
+    func()
