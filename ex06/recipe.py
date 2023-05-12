485c40f5499e6dc856929d13799c4291276b3abb
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-12 09:27:08 +0200

    Add function to print a recipe's details

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -19,3 +19,21 @@ cookbook = {
 def print_recipe_names():
     for k in cookbook.keys():
         print(k)
+
+def print_recipe_details():
+    print("Please enter a recipe name to get its details:")
+    name = input(">> ")
+
+    recipe = cookbook.get(name)
+
+    if recipe is None:
+        print("invalid recipe")
+
+    ingredients = recipe["ingredients"]
+    meal = recipe["meal"]
+    prep_time = recipe["prep_time"]
+
+    print(f"Recipe for {name}:")
+    print(f"  Ingredients list: {recipe}")
+    print(f"  To be eaten for {meal}.")
+    print(f"  Takes {prep_time} minutes of cooking.")
