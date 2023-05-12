2c5d0712428298bf506a1fd3657100eaa241628e
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-12 09:05:28 +0200

    Add kata01

---
+++ ex05/kata01.py
@@ -0,0 +1,8 @@
+kata = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+        }
+
+for k, v in kata.items():
+    print(f'{k} was created by {v}')
