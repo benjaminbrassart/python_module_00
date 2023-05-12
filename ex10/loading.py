3004b6bace00ab9b90883a0fbb949b5d0b63aa86
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-12 10:51:40 +0200

    Add arrow Add percentage Add current/total

--- ex10/loading.py
+++ ex10/loading.py
@@ -1,16 +1,26 @@
 from time import sleep, time
+from math import floor
 
 LINE_CLEAR = "\x1b[2K"
 
 def ft_progress(it):
     start_time = time()
     last_time = start_time
+    count_current = 0
     count_total = len(it)
 
     for elem in it:
+        count_current += 1
+        ratio = count_current / count_total
+        percentage = round(ratio * 100)
         elapsed = time() - start_time
 
-        print(f"| elapsed time {elapsed:.2f}s", end="\r")
+        arrow_len = floor(25 * ratio)
+        if ratio == 1:
+            arrow_len -= 1
+        arrow = (("=" * arrow_len) + ">").ljust(25, " ")
+
+        print(f" [{percentage:3d}%] [{arrow}] {count_current}/{count_total} | elapsed time {elapsed:.2f}s", end="\r")
         # print(end=LINE_CLEAR)
         yield elem
 
