bdf124b1d2092c24ddbc32f95397256326493a3b
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-12 11:21:33 +0200

    Add ETA

--- ex10/loading.py
+++ ex10/loading.py
@@ -2,6 +2,7 @@ from time import sleep, time
 from math import floor
 
 LINE_CLEAR = "\x1b[2K"
+ARROW_SIZE = 25
 
 def ft_progress(it):
     start_time = time()
@@ -13,15 +14,18 @@ def ft_progress(it):
         count_current += 1
         ratio = count_current / count_total
         percentage = round(ratio * 100)
-        elapsed = time() - start_time
+        current_time = time()
+        elapsed = current_time - start_time
+        eta = (count_total - count_current) * (current_time - last_time)
+        last_time = current_time
 
-        arrow_len = floor(25 * ratio)
+        arrow_len = floor(ARROW_SIZE * ratio)
         if ratio == 1:
             arrow_len -= 1
-        arrow = (("=" * arrow_len) + ">").ljust(25, " ")
+        arrow = (("=" * arrow_len) + ">").ljust(ARROW_SIZE, " ")
 
-        print(f" [{percentage:3d}%] [{arrow}] {count_current}/{count_total} | elapsed time {elapsed:.2f}s", end="\r")
-        # print(end=LINE_CLEAR)
+        print(end=LINE_CLEAR)
+        print(f"ETA: {eta:.2f}s [{percentage:3d}%] [{arrow}] {count_current}/{count_total} | elapsed time {elapsed:.2f}s", end="\r")
         yield elem
 
 if __name__ == "__main__":
